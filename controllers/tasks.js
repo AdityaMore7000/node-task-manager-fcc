3fc563cadd123aadaae3c00958bd12e7ce7ef666
adityamore7000 <adi.more7000@gmail.com>
2023-12-14 23:03:03 +0530

    add logs

--- controllers/tasks.js
+++ controllers/tasks.js
@@ -1,16 +1,21 @@
 function getAllTasks(req,res){
+    console.log(req.method)
     res.json({msg:'here are all your tasks'})
 }
 function createTask(req,res){
+    console.log(req.method)
     res.json({msg:'Created new task'})
 }
 function getTask(req,res){
+    console.log(req.method)
     res.json({msg:'one task'})
 }
 function updateTask(req,res){
+    console.log(req.method)
     res.json({msg:'updated task'})
 }
 function deleteTask(req,res){
+    console.log(req.method)
     res.json({msg:'deleted task'})
 }
 module.exports = {
